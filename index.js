a063fd6a15aaf6f5ebf15b4ae39d1dd19dded631
MFerguson1449 <MFerguson1449@gmail.com>
2019-02-08 14:51:11 -0500

    new style, new icon

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 <script>
     document.body.classList.add('fade');
 </script>
+
+function toggle() {
+    var button = document.querySelector('.toggle');
+    var overlay = document.querySelector('.glass');
+    if (overlay.className === 'glass down') {
+      overlay.className = 'glass up';
+      button.innerText = '-';
+    } else {
+      overlay.className = 'glass down';
+      button.innerText = '+';
+    }
+  }
